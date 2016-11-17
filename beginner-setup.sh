0ce423e479170aaef6065521c83adeaee992185d
claudiopro <claudio.procida@gmail.com>
2016-11-17 17:53:42 +0000

    Adds beginner and intermediate setup scripts

---
+++ beginner-setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+docker pull hello-world 
+docker pull alpine 
+docker pull seqvence/static-site
---
+++ intermediate-setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+docker pull microsoft/dotnet:1.0.0-preview1 
+docker pull node:5.11.0-slim 
+docker pull python:2.7-alpine 
+docker pull redis:alpine
+docker pull postgres:9.4
